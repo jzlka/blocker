0732faaba3560a5d94254c40fb58fa383bddc3fd
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2020-05-22 00:21:47 +0200

    sign.sh variables enclosed in brackets

--- blocker/sign.sh
+++ blocker/sign.sh
@@ -2,14 +2,14 @@
 APP="blockerd"
 PATH_TO_APP=$APP
 ENTITLEMENTS="${APP}.entitlements"
-CERT_ID="j.kulik128@gmail.com"
+CERT_ID="-"
 
 echo "\n\n**** Previous entitlements of $APP ****"
-codesign -d --ent :-  "$PATH_TO_APP/$APP"
+codesign -d --ent :-  "${PATH_TO_APP}/${APP}"
 
 
 echo "\n\n**** Signing $APP ****"
-codesign --force -vvvv --entitlements "$ENTITLEMENTS" -s "$CERT_ID" "$PATH_TO_APP/$APP"
+codesign --force -vvvv --entitlements "$ENTITLEMENTS" -s "$CERT_ID" "${PATH_TO_APP}/${APP}"
 
 echo "\n\n**** Final entitlements of $APP ****"
-codesign -d --ent :-  "$PATH_TO_APP/$APP"
+codesign -d --ent :-  "${PATH_TO_APP}/${APP}"
