63154f3c209645bd02a88ad60587d34f05840e2f
Jozef Zuzelka <jozef.zuzelka@gmail.com>
2020-05-21 13:44:54 +0200

    Added makefile and signing script

---
+++ blocker/sign.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+APP="blockerd"
+PATH_TO_APP=$APP
+ENTITLEMENTS="${APP}.entitlements"
+CERT_ID="j.kulik128@gmail.com"
+
+echo "\n\n**** Previous entitlements of $APP ****"
+codesign -d --ent :-  "$PATH_TO_APP/$APP"
+
+
+echo "\n\n**** Signing $APP ****"
+codesign --force -vvvv --entitlements "$ENTITLEMENTS" -s "$CERT_ID" "$PATH_TO_APP/$APP"
+
+echo "\n\n**** Final entitlements of $APP ****"
+codesign -d --ent :-  "$PATH_TO_APP/$APP"
